849580eed062b2c576e1d00f18f34e6ed689bf38
usabucket <usabucket@gmail.com>
2014-04-14 15:53:55 +0800

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '4d78af9a7a758f74cad9c50c65ef773a257600a19bb0ddb2468446188b47815baa575edfca83331eba546fc283981aaf8a1e1024559adfd39e17d3d7e8d51a33'
